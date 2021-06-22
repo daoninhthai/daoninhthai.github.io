3a9aa06cca1a1cd44e0d84d8c3930720174b9077
daoninhthai <thaimeo1131@gmail.com>
2021-06-22 11:15:19 +0700

    fix-empty-search-box

--- Nashtech/FE/B1/main.js
+++ Nashtech/FE/B1/main.js
@@ -1,6 +1,11 @@
 $(document).ready(function() {
 
     $("#search-button").click(function() {
-        $("#searching").show();
+        if (document.getElementById("searchbox").value == "") {
+            alert("[Chưa nhập cái gì sao search ><]");
+        } else {
+            $("#searching").show();
+            setTimeout(function() { $("#searching").hide(); }, 3000);
+        }
     });
 });
